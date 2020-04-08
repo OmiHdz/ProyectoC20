b2ff3fd7e4b4f99ab30e5010edb968a1e278a174
Omar Saucedo <omaroso27@gmail.com>
2020-04-07 21:25:20 -0500

    [IMP] polyline function

--- js/mapFunctions.js
+++ js/mapFunctions.js
@@ -1,5 +1,13 @@
 let myMap;
-let startPosition = {lat: 20.675377, lng: -103.340121};
+
+// hardcoded values
+let startPosition = { lat: 20.675377, lng: -103.340121 };
+let routeCoordinates = [
+	{ lat: 20.665377, lng: -103.330121 },
+	{ lat: 20.705377, lng: -103.350121 },
+	{ lat: 20.695377, lng: -103.460121 },
+	{ lat: 20.505377, lng: -103.370121 }
+];
 
 function initMap() {
 	var mapProp = {
@@ -16,11 +24,47 @@ function initMap() {
 		myMap.addListener('click', (e) => {
 			placeMarker(e.latLng, myMap);
 		});
+
+		// markerArray = [];
+		// for(coord of routeCoordinates) {
+		// 	markerArray.push(placeMarker(coord, myMap));
+		// }
+		// drawPolyline(markerArray, myMap);
+		drawPolyline(routeCoordinates, myMap);
 	}
+
 }
 
 // returns the new marker but it must be handled wherever this function is called
 function placeMarker(latLng, map) {
 	return new google.maps.Marker({position: latLng, map: map});
 }
+
+
+// function accepts array of coordinates or array of markers
+function drawPolyline(points = [], map) {
+	let pathPoints;
+	if (points[0].getPosition) {
+		pathPoints = points.map(point => {
+			console.log(point.getPosition());
+			return point.getPosition();
+		});
+	}
+	else {
+		pathPoints = points;
+	}
+	var path = new google.maps.Polyline({
+		path: pathPoints,
+		geodesic: true,
+		strokeColor: '#FF0000',
+		strokeOpacity: 1.0,
+		strokeWeight: 2
+	});
+
+	path.setMap(map);
+	return path;
+}
+
+
+
 
